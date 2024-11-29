b0f15a9084fd7c71c38f73db75b215f6bc8edf18
Nontanan <nantanan_s@cmu.ac.th>
2024-11-29 15:46:44 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int x ;
+    int a = 0 ;
+    int b = 0 ;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    while( x!=0 ){
+        if(x%2==0)
+        {a=a+1;}
+        else{b=b+1;}
+    cout << "Enter an integer: ";
+    cin >> x;
+
+    }
+    cout << "#Even numbers = " << a << endl;
+    cout << "#Odd numbers = " << b << endl;
     return 0;
 }
